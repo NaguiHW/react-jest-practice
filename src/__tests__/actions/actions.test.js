ce0c6cc621c3596d7a5736fe8e154f087ce3aa39
Kalib Hackin <nagui08@hotmail.com>
2020-04-27 10:59:00 -0500

    actions tested

---
+++ src/__tests__/actions/actions.test.js
@@ -0,0 +1,22 @@
+import actions from '../../actions';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Test Actions', () => {
+  it('addToCart action', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+    expect(actions.addToCart(payload)).toEqual(expected);
+  });
+
+  it('removeFromCart action', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    expect(actions.removeFromCart(payload)).toEqual(expected);
+  });
+});
